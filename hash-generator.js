c85f827a825994518d9b2933980210760c1e1ba1
Azilske <angela.zilske@gmail.com>
2025-05-28 12:51:32 +0000

    🔧 Entfernt: hash.js und hash-generator.js, da nicht mehr benötigt

--- hash-generator.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const plainPassword = 'j0hnny22';
-
-bcrypt.hash(plainPassword, 10).then((hash) => {
-  console.log('🔐 Hash für j0hnny22:');
-  console.log(hash);
-});
